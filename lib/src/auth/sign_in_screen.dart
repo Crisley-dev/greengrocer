92af1d298f4876a80d17d68c9240f80095d92c91
CRISLEY DUARTE <cryslley17@gmail.com>
2024-01-01 18:53:33 -0300

    Terminando a parte de visual do form de login e criando os botoes restantes

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -10,31 +10,100 @@ class SignInScreen extends StatelessWidget {
       backgroundColor: Colors.green,
       body: Column(
         children: [
+          // Container Verde do Topo
           Expanded(
             child: Container(
               color: Colors.green,
             ),
           ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius:
-                      BorderRadius.vertical(top: Radius.circular(45))),
-              child: const Column(
-                children: [
-                  CustomTextField(
-                    icon: Icons.email,
-                    label: 'Email',
+
+          //Container branco de Baixo
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(45))),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                //Email
+                const CustomTextField(
+                  icon: Icons.email,
+                  label: 'Email',
+                ),
+
+                //Senha
+                const CustomTextField(
+                  icon: Icons.lock,
+                  label: 'Senha',
+                  isSecret: true,
+                ),
+
+                //Botão Entrar
+                SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15))),
+                        child: const Text(
+                          'Entrar',
+                          style: TextStyle(color: Colors.white),
+                        ))),
+
+                //Esqueceu a Senha
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text('Esqueceu a Senha?',
+                        style: TextStyle(color: Colors.red)),
                   ),
-                  CustomTextField(
-                    icon: Icons.lock,
-                    label: 'Senha',
-                    isSecret: true,
-                  )
-                ],
-              ),
+                ),
+                //Divisor
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Text('OU'),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // Botão de Novo usuario
+
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton(
+                      onPressed: () {},
+                      style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18)),
+                          side:
+                              const BorderSide(width: 2, color: Colors.green)),
+                      child: const Text(
+                        'Criar Conta',
+                        style: TextStyle(fontSize: 18, color: Colors.green),
+                      )),
+                )
+              ],
             ),
           )
         ],
