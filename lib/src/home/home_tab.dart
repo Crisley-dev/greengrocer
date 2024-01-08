04016c3d7b066415222ac0628531184e4aa81ae1
CRISLEY DUARTE <cryslley17@gmail.com>
2024-01-08 16:01:14 -0300

    Criação do componente category tile e impementando aba categoria na hometab

--- lib/src/home/home_tab.dart
+++ lib/src/home/home_tab.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_color.dart';
+import 'package:greengrocer/src/home/components/category_tile.dart';
 
-class HomeTab extends StatelessWidget {
+class HomeTab extends StatefulWidget {
   const HomeTab({super.key});
+
+  @override
+  State<HomeTab> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  List<String> categories = [
+    'Frutas',
+    'Grãos',
+    'Verduras',
+    'Temperos',
+    'Cereais',
+  ];
+
+  String selectedCategory = 'Frutas';
 
   @override
   Widget build(BuildContext context) {
@@ -81,13 +97,34 @@ class HomeTab extends StatelessWidget {
                         style: BorderStyle.none,
                       ))),
             ),
+          ),
+
+          //Categorias
+          Container(
+            padding: const EdgeInsets.only(left: 25),
+            height: 40,
+            child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (_, index) {
+                  return CategoryTile(
+                    onPressed: () {
+                      setState(() {
+                        selectedCategory = categories[index];
+                      });
+                    },
+                    category: categories[index],
+                    isSelected: categories[index] == selectedCategory,
+                  );
+                },
+                separatorBuilder: (_, index) => const SizedBox(
+                      width: 10,
+                    ),
+                itemCount: categories.length),
           )
+
+          //Grid
         ],
       ),
-
-      //Categorias
-
-      //Grid
     );
   }
 }
