2edac94a745e7ac446a18be0873c3d88ac06edd2
CRISLEY DUARTE <cryslley17@gmail.com>
2024-01-09 16:59:09 -0300

    grid de imagem completo com nome, preço e botao de add no carrinho

---
+++ lib/src/pages/home/components/item_tile.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_color.dart';
+import 'package:greengrocer/src/models/item_model.dart';
+import 'package:greengrocer/src/services/util_services.dart';
+
+class ItemTile extends StatelessWidget {
+  ItemTile({super.key, required this.item});
+
+  final ItemModel item;
+  final UtilsServices utilsServices = UtilsServices();
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Card(
+          elevation: 1,
+          shadowColor: Colors.grey.shade300,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  //imagem
+                  Expanded(
+                    child: Image.asset(item.imgUrl),
+                  ),
+
+                  //nome
+                  Text(
+                    item.itemName,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  //preço - unidade
+                  Row(
+                    children: [
+                      Text(
+                        utilsServices.priceToCurrency(item.price),
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: CustomColors.customSwatchColor,
+                        ),
+                      ),
+                      Text(
+                        '/${item.unit}',
+                        style: TextStyle(
+                          color: Colors.grey.shade500,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                        ),
+                      )
+                    ],
+                  )
+                ]),
+          ),
+        ),
+        Positioned(
+            top: 4,
+            right: 4,
+            child: GestureDetector(
+              onTap: () {},
+              child: Container(
+                height: 40,
+                width: 35,
+                decoration: BoxDecoration(
+                    color: CustomColors.customSwatchColor,
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(15),
+                      topRight: Radius.circular(20),
+                    )),
+                child: const Icon(
+                  Icons.add_shopping_cart_outlined,
+                  color: Colors.white,
+                  size: 20,
+                ),
+              ),
+            ))
+      ],
+    );
+  }
+}
--- lib/src/pages/home/home_tab.dart
+++ lib/src/pages/home/home_tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/app_data.dart' as app_data;
 import 'package:greengrocer/src/config/custom_color.dart';
 import 'package:greengrocer/src/pages/home/components/category_tile.dart';
+import 'package:greengrocer/src/pages/home/components/item_tile.dart';
 
 class HomeTab extends StatefulWidget {
   const HomeTab({super.key});
@@ -129,8 +130,8 @@ class _HomeTabState extends State<HomeTab> {
               ),
               itemCount: app_data.items.length,
               itemBuilder: (_, index) {
-                return Container(
-                  color: Colors.red,
+                return ItemTile(
+                  item: app_data.items[index],
                 );
               },
             ),
